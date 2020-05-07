2bf1e419d58c57eadc9783804449f70185c95e1c
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-05-07 07:51:38 +0200

    cleanup

--- src/main/java/com/alex/instokram/domain/User.java
+++ src/main/java/com/alex/instokram/domain/User.java
@@ -3,14 +3,11 @@ package com.alex.instokram.domain;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name = "user")
 @XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 @Data
 public class User {
     @Id
--- src/main/java/com/alex/instokram/domain/UserUploadedImage.java
+++ src/main/java/com/alex/instokram/domain/UserUploadedImage.java
@@ -3,14 +3,11 @@ package com.alex.instokram.domain;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name = "useruploadedimage")
 @XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 @Data
 public class UserUploadedImage {
 
